6e200e73400507e210a5b978414814846444e0c6
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-14 16:24:17 +0000

    ksdjfhdfg

--- DesignPatterns/_Facade/Customer.cs
+++ DesignPatterns/_Facade/Customer.cs
@@ -6,11 +6,13 @@ namespace DesignPatterns._Facade
     {
         public string customerName;
 
+        // Constructor
         public Customer(string customerName)
         {
             this.customerName = customerName;
         }
 
+        // Gets the name
         public string CustomerName
         {
             get { return customerName; }
